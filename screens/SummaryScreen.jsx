128cfb5ca6e86587513a4fe7af27c27d16a984e7
Silas <63477128+SilasStokes@users.noreply.github.com>
2023-03-29 09:59:26 -0700

    added scrollable calendar

--- screens/SummaryScreen.jsx
+++ screens/SummaryScreen.jsx
@@ -1,7 +1,14 @@
-import { View, Button, Text } from "react-native";
+import {
+  View,
+  Button,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 // import calendar from react native calender
-import { Calendar, LocaleConfig } from "react-native-calendars";
+import { Calendar, CalendarList } from "react-native-calendars";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import dates from "../helper/data.js";
 
@@ -14,12 +21,20 @@ export default function SummaryScreen({ route, navigation }) {
     // console.log(color.color);
     const yyyy_mm_dd = color.date.substring(0, 10);
     coloredDates[yyyy_mm_dd] = {
-      selected: true,
-      //   marked: true,
+      // create a customStyles object that sets the entire available space to the color.color
+      customStyles: {
+        container: {
+          backgroundColor: color.color,
+          borderRadius: 5,
+        },
+        text: {
+          color: "black",
+        },
+      },
+
+      // selected: true,
       disableTouchEvent: true,
-      //   dotColor: color.color,
-      selectedColor: color.color,
-      //   selectedDotColor: color.color,
+      // selectedColor: color.color,
     };
   });
   //   colors.forEach((color, index) => {
@@ -31,8 +46,8 @@ export default function SummaryScreen({ route, navigation }) {
   //     };
   //   });
 
+  // getting the colors from AsyncStorage
   useEffect(() => {
-    // create an async immediate function
     (async () => {
       try {
         const jsonStr = await AsyncStorage.getItem("date_color");
@@ -43,11 +58,9 @@ export default function SummaryScreen({ route, navigation }) {
         console.log(e);
       }
     })();
-
-    //   return () => {
-    //   }
   }, []);
 
+  // check to see if CalendarList has loaded, if not, display a message
   if (!colors) {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -55,18 +68,41 @@ export default function SummaryScreen({ route, navigation }) {
       </View>
     );
   }
+  const today = new Date();
+  const currentMonth = today.getMonth();
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Calendar
-        disableMonthChange={true}
-        hideArrows={true}
-        onDayPress={(day) => {
-          setSelected(day.dateString);
-        }}
-        markedDates={coloredDates}
-      />
-    </View>
+    <CalendarList
+      // Callback which gets executed when visible months change in scroll view. Default = undefined
+      onVisibleMonthsChange={(months) => {
+        console.log("now these months are visible", months);
+      }}
+      // Max amount of months allowed to scroll to the past. Default = 50
+      pastScrollRange={currentMonth}
+      // Max amount of months allowed to scroll to the future. Default = 50
+      futureScrollRange={11 - currentMonth}
+      // Enable or disable scrolling of calendar list
+      scrollEnabled={true}
+      // Enable or disable vertical scroll indicator. Default = false
+      showScrollIndicator={true}
+      markingType={"custom"}
+      markedDates={coloredDates}
+    />
+  );
+
+  return (
+    <SafeAreaView style={{ paddingTop: StatusBar.currentHeight }}>
+      <ScrollView style={{}}>
+        <Calendar
+          disableMonthChange={true}
+          hideArrows={true}
+          onDayPress={(day) => {
+            setSelected(day.dateString);
+          }}
+          markedDates={coloredDates}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 
   return (
